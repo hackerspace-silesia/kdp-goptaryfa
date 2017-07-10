d5c4622d880cab3d473d1822a955694a34345db7
sleaz0id <mat.staszczyk@gmail.com>
2017-07-10 19:21:04 +0200

    Extend table method added

--- tickets.js
+++ tickets.js
@@ -1,4 +1,28 @@
-function TicketAdvisor (timetable) {
-  this.timetable = timetable
+function TicketAdvisor(timetable) {
+  this.timetable = $(timetable)
 }
 
+TicketAdvisor.prototype.extendTableHeader = function() {
+  var col_header = this.timetable.find("thead th").first();
+  var checkbox_header = $("<th></th>");
+  col_header.before(checkbox_header);
+}
+
+TicketAdvisor.prototype.extendTableBody = function() {
+	var rows = this.timetable.find("tbody tr:not([id])");
+  var id_string = "chbox_stop_id";
+  var cls_stops = "chbox_stops";
+  var rowInput = null;
+
+  for (var i = 0; i < rows.length; i++) {
+    rowInput = "<td style=\"text-align: center;\">" +
+			   "<input class=\"" + cls_stops + "\" id=\"" + id_string + i +
+			   "\" type=\"checkbox\"></td>";
+    $(rowInput).prependTo(rows[i]);
+  }
+}
+
+TicketAdvisor.prototype.extendTable = function() {
+  this.extendTableHeader();
+  this.extendTableBody();
+}
