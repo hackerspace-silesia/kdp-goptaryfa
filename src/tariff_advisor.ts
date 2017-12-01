ff7d76fd1d060867bf02e0744565f36f43be6dba
jerryskye <jarek@siedlarz.com>
2017-12-01 21:28:50 +0100

    cosmetic changes

--- src/tariff_advisor.ts
+++ src/tariff_advisor.ts
@@ -17,20 +17,20 @@ export class TariffAdvisor {
 
         const [startStopId, endStopId] = [...checkedStopsIds];
         const result = {
-            time: 0, 
-            distance: 0, 
+            time: 0,
+            distance: 0,
             zones: new Set()
-        }; 
+        };
         const chosenStops = stops.filter(stop => {
             return (stop['data-stop'] >= startStopId && stop['data-stop'] <= endStopId)
-        
+
         });
         chosenStops.forEach(stop => {
             result.time += stop.time;
             result.distance += stop.distance;
             result.zones.add(stop.zone);
         });
-        
+
         console.table(result);
 
         return Tariff.Time;
